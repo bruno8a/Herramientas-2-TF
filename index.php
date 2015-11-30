3e8748281537f2f01ef31e5a5ef2ba1b95a6cd0e
DobleClick <DobleClick@B8>
2015-11-30 16:32:38 -0300

    #1->datos

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "BRUNO OCHOA";
+        echo "DNI:35826079";
+        echo "LU:2943";
+        echo "BRUNO8A_10@HOTMAIL.COM" 
+        ?>
+    </body>
+</html>
